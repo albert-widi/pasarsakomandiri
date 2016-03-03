5eeb442b9cb64b7feab30ec5d0915fe2e9109cde
albert-widi <albert.widiatmoko@gmail.com>
2016-03-04 01:59:06 +0700

    Add save to static FS

--- shared/static/static.go
+++ shared/static/static.go
@@ -2,6 +2,10 @@ package static
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"io/ioutil"
+"log"
+	"os"
+	"errors"
 )
 
 type Static struct {
@@ -13,10 +17,26 @@ type Static struct {
 	CameraURI string		`json:"CameraURI"`
 }
 
+var stat Static
+
 func Configure(r *gin.Engine, s Static) {
 	//set statics
+	stat = s
 	//r.Static("/testassets", "assets")
 	r.StaticFS(s.StaticAssets, http.Dir(s.AssetsURI))
 	r.StaticFS(s.StaticPage, http.Dir(s.PageURI))
 	r.StaticFS(s.CameraFile, http.Dir(s.CameraURI))
+}
+
+func SaveFileToStaticFS(fileType string, file []byte, fileName string) error {
+	switch fileType {
+		case "camera":
+			var err error
+			err = ioutil.WriteFile(stat.CameraURI+string(os.PathSeparator)+fileName, file, 0666)
+
+			if err != nil {
+				log.Println(err)
+			}
+			return err
+	}
 }
