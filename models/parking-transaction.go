18a2f6b0222df6b9c99952f386033f045e40df05
albert-widi <albert.widiatmoko@gmail.com>
2016-03-22 01:54:24 +0700

    Picture in parking transaction + safe random

--- models/parking-transaction.go
+++ models/parking-transaction.go
@@ -1,5 +1,8 @@
 package models
-import "github.com/pasarsakomandiri/shared/database"
+
+import (
+	"github.com/pasarsakomandiri/shared/database"
+)
 
 func ParkingTransactionGetAllAPI(condition string) ([]ParkingTicket, error) {
 	parkingTicket := []ParkingTicket{}
@@ -9,8 +12,20 @@ func ParkingTransactionGetAllAPI(condition string) ([]ParkingTicket, error) {
 		condition = "1=1"
 	}
 
-	err := database.Db.Select(&parkingTicket, "SELECT id, ticket_number, vehicle_id, vehicle_type, COALESCE(vehicle_number, ' ') vehicle_number, COALESCE(out_date, ' ') out_date, COALESCE(parking_cost, 0) parking_cost, COALESCE(verified_by, 0) verified_by, created_date, created_by, COALESCE(last_update_date, ' ') last_update_date, COALESCE(updated_by, 0) updated_by FROM parking_transactions WHERE "+condition)
-
+	queryString := "SELECT id" +
+			", ticket_number" +
+			", vehicle_id" +
+			", vehicle_type" +
+			", COALESCE(vehicle_number, ' ') vehicle_number" +
+			", COALESCE(out_date, ' ') out_date" +
+			", COALESCE(parking_cost, 0) parking_cost" +
+			", COALESCE(verified_by, 0) verified_by" +
+			", created_date, created_by, COALESCE(last_update_date, ' ') last_update_date, COALESCE(updated_by, 0) updated_by" +
+			", COALESCE((SELECT CONCAT('/', pic.filepath, '/', pic.filename, '.', pic.format) FROM pictures pic WHERE pic.id = picture_in_id), ' ') picture_path_in" +
+			", COALESCE((SELECT CONCAT('/', pic.filepath, '/', pic.filename, '.', pic.format) FROM pictures pic WHERE pic.id = picture_out_id), ' ') picture_path_out" +
+			" FROM parking_transactions WHERE " + condition
+	//err := database.Db.Select(&parkingTicket, "SELECT id, ticket_number, vehicle_id, vehicle_type, COALESCE(vehicle_number, ' ') vehicle_number, COALESCE(out_date, ' ') out_date, COALESCE(parking_cost, 0) parking_cost, COALESCE(verified_by, 0) verified_by, created_date, created_by, COALESCE(last_update_date, ' ') last_update_date, COALESCE(updated_by, 0) updated_by FROM parking_transactions WHERE "+condition)
+	err := database.Db.Select(&parkingTicket, queryString)
 	return parkingTicket, err
 }
 
--- shared/tools/rand.go
+++ shared/tools/rand.go
@@ -1,13 +1,20 @@
 package tools
-import "math/rand"
+
+import (
+	"math/rand"
+	"sync"
+)
 
 var runes  =  []rune("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var mux sync.Mutex
 
 func RandomString(l int) string {
 	runesLen := len(runes)
 	bytes := make([]rune, l)
 	for i:=0 ; i<l ; i++ {
+		mux.Lock()
 		bytes[i] = runes[rand.Intn(runesLen)]
+		mux.Unlock()
 	}
 	return string(bytes)
 }
